b14a3ce170075ec38dab315e3570dee8b105c260
Stephen Gambill <115902028+StephenSGambill@users.noreply.github.com>
2023-11-09 09:45:39 -0600

    catch up commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_03_160950) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_06_192309) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
     t.string "email", default: "", null: false
